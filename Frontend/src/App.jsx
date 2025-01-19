5c9ad9867ff6981c3e07ee3d4749c6111efbf7b8
unknown <koundal.anshuk2004@gmail.com>
2025-01-19 23:13:17 +0530

    Front-end

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignup from "./Pages/CaptainSignup";
+import Start from "./pages/Start";
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Start/>}></Route>
+        <Route path="/login" element={<UserLogin/>}></Route>
+        <Route path="/signup" element={<UserSignup/>}></Route>
+        <Route path="/captain-login" element={<CaptainLogin/>}></Route>
+        <Route path="/captain-signup" element={<CaptainSignup/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+
+export default App;
